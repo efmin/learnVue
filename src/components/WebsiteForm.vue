4a327521ef80560748b84375cf8586e6e16643e7
EFFY MIN <effyverse@gmail.com>
2020-06-10 10:07:55 -0400

    create website add form component

---
+++ src/components/WebsiteForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="employee-form">
+    <form>
+      <label>Website name</label>
+      <input type="text" />
+      <label>Website Email</label>
+      <input type="text" />
+      <button>Add Website</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'website-form',
+    data() {
+      return {
+        website: {
+          name: '',
+          url: '',
+        },
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
