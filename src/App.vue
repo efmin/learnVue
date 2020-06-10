9b6a02ec58a644e656f54d7eb0317cb5b895d066
EFFY MIN <effyverse@gmail.com>
2020-06-10 09:48:15 -0400

    register and use website tables component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Websites</h1>
+
+    <website-table />
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WebsiteTable from '@/components/WebsiteTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    WebsiteTable
+  },
 }
 </script>
 
+
 <style>
+  button {
+    background: #009435;
+    border: 1px solid #009435
+  }
+
+  .small-countainer {
+    max-width: 680px;
+  }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +37,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/WebsiteTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="website-table">
+    <table>
+        <thead>
+          <tr>
+            <th>Website Name</th>
+            <th>Website URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>New York Times</td>
+            <td>https://www.nytimes.com</td>
+          </tr>
+          <tr>
+            <td>Financial Times</td>
+            <td>https://www.ft.com</td>
+          </tr>
+          <tr>
+            <td>Aljazeera</td>
+            <td>https://www.aljazeera.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'website-table',
+  }
+</script>
+
+<style scoped></style>
